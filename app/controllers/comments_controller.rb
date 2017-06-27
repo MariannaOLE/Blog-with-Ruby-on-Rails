561acf99c1388caf68b48f10db1e2e75fc492175
MariannaOLE <marianna.olefyrenko@gmail.com>
2017-06-27 20:21:14 +0800

    Article editing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,14 @@ class CommentsController < ApplicationController
 
   redirect_to article_path(@comment.article)
 end
+def destroy
+  @comment = Comment.find(params[:id])
+  @comment.destroy
+  flash.notice = "Article '#{@article.title}' Deleted!"
+
+  redirect_to articles_path
+
+end
 
 def comment_params
   params.require(:comment).permit(:author_name, :body)
